94329c72d21378eb762bf549cc75d6f9873910b6
unknown <orsolyadk@gmail.com>
2022-12-15 01:26:19 +0100

    User creation added to orchestrator.

--- AddRestaurantOrchestration.cs
+++ AddRestaurantOrchestration.cs
@@ -6,6 +6,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Restaurant;
 
@@ -18,14 +19,23 @@ public static class AddRestaurantOrchestration
   {
     log.LogInformation("AddRestaurantOrchestration function processed a request.");
 
-    RestaurantItem adat = context.GetInput<RestaurantItem>();
+    Restaurant data = context.GetInput<Restaurant>();
 
-    var outputs = new List<string>();
+    var RId = await context.CallActivityAsync<string>("AddRestaurantActivity", data.RestaurantItem);
+    var order = await context.CallHttpAsync(HttpMethod.Post, new System.Uri("https://orders-func-app.azurewebsites.net/api/addorderrestaurant?rid=" + RId));
+    var userData = new {
+      restaurant = RId,
+      username = data.RestaurantItem.Name,
+      email = data.RestaurantItem.Email,
+      password = data.Password
+    };
+    string userJson = JsonConvert.SerializeObject(userData);
+    var user = await context.CallHttpAsync(HttpMethod.Post, new System.Uri("https://auth-func-app.azurewebsites.net/api/adduser"), userJson);
 
-    var RId = await context.CallActivityAsync<string>("AddRestaurantActivity", adat);
-    var added = await context.CallHttpAsync(HttpMethod.Post, new System.Uri("https://orders-func-app.azurewebsites.net/api/addorderrestaurant?rid=" + RId));
     log.LogInformation($"AddRestaurantOrchestration ended.");
-
+    log.LogInformation("Restaurant response: " + RId);
+    log.LogInformation("Order response: " + order.StatusCode + " " + order.Content);
+    log.LogInformation("User response: " + user.StatusCode + " " + user.Content);
     return RId;
   }
 
@@ -41,7 +51,7 @@ public static class AddRestaurantOrchestration
     log.LogInformation("AddRestaurant activity function processed a request.");
 
     // name can not be data (throws error)
-    RestaurantItem newRestaurant = rData;
+    RestaurantItem newRestaurant = rData;    
 
     // create a random ID
     var RId = System.Guid.NewGuid().ToString();
@@ -62,7 +72,7 @@ public static class AddRestaurantOrchestration
       [DurableClient] IDurableOrchestrationClient starter,
       ILogger log)
   {
-    RestaurantItem data = await req.Content.ReadAsAsync<RestaurantItem>();
+    Restaurant data = await req.Content.ReadAsAsync<Restaurant>();
 
     // Function input comes from the request content.
     string instanceId = await starter.StartNewAsync("AddRestaurantOrchestration", data);
--- Restaurant.cs
+++ Restaurant.cs
@@ -1,60 +1,66 @@
 namespace Restaurant;
 
 #nullable enable
+public class Restaurant
+{
+    public RestaurantItem RestaurantItem { get; set; }
+    public string Password { get; set; }
+}
+
 public class RestaurantItem
 {
-  public string? partitionKey { get; set; }
-  public string? id { get; set; }
-  public string? Name { get; set; }
-  public string? Address { get; set; }
-  public string? Phone { get; set; }
-  public string? Email { get; set; }
-  public bool Takeaway { get; set; }
-  public Map? Tables { get; set; }
-  public int Pricing { get; set; }
-  public string? CardNum { get; set; }
-  public int OpeningH { get; set; }
-  public int OpeningM { get; set; }
-  public int ClosingH { get; set; }
-  public int ClosingM { get; set; }
-  public MenuItem[]? Menu { get; set; }
-
-  public string? Image { get; set; }
-  public string? Website { get; set; }
-  public string? Style { get; set; }
-  public Review[]? Reviews { get; set; }
-  public int? Rating { get; set; }
-
-  public class MenuItem
-  {
-    public string? dishId { get; set; }
-    public string? Name { get; set; }
-    public string? Description { get; set; }
-    public int Price { get; set; }
-    public string? Image { get; set; }
-  }
-
-  public class Review
-  {
+    public string? partitionKey { get; set; }
     public string? id { get; set; }
-    public string? Date { get; set; }
-    public int Rating { get; set; }
-    public string? Text { get; set; }
-    public string? Answer { get; set; }
-  }
-
-  public class Map
-  {
     public string? Name { get; set; }
-    public int Size { get; set; }
+    public string? Address { get; set; }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    public bool Takeaway { get; set; }
+    public Map? Tables { get; set; }
+    public int Pricing { get; set; }
+    public string? CardNum { get; set; }
+    public int OpeningH { get; set; }
+    public int OpeningM { get; set; }
+    public int ClosingH { get; set; }
+    public int ClosingM { get; set; }
+    public MenuItem[]? Menu { get; set; }
+
     public string? Image { get; set; }
-    public Field[][]? Fields { get; set; }
-  }
+    public string? Website { get; set; }
+    public string? Style { get; set; }
+    public Review[]? Reviews { get; set; }
+    public int? Rating { get; set; }
+
+    public class MenuItem
+    {
+        public string? dishId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int Price { get; set; }
+        public string? Image { get; set; }
+    }
+
+    public class Review
+    {
+        public string? id { get; set; }
+        public string? Date { get; set; }
+        public int Rating { get; set; }
+        public string? Text { get; set; }
+        public string? Answer { get; set; }
+    }
+
+    public class Map
+    {
+        public string? Name { get; set; }
+        public int Size { get; set; }
+        public string? Image { get; set; }
+        public Field[][]? Fields { get; set; }
+    }
 }
 
 public class Field
 {
-  public string? Type { get; set; }
-  public int Data { get; set; }
-  public (int, int)[]? Reserved { get; set; }
+    public string? Type { get; set; }
+    public int Data { get; set; }
+    public (int, int)[]? Reserved { get; set; }
 }
